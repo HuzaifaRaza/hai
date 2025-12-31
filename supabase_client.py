638d018ec4c9ca6671582c62927b0bb55876f0ba
Huzaifa Raza <hrha80808@gmail.com>
2025-12-31 18:11:13 +0500

    Update supabase_client.py

--- supabase_client.py
+++ supabase_client.py
@@ -1,1 +1,9 @@
+from supabase import create_client
+import streamlit as st
+
+# Ye credentials Streamlit Cloud secrets me honi chahiye
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
+SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
+
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
